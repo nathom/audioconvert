3deec293b23e045165c0ad52aa343445aab79c48
pynathanthomas <74019033+pynathanthomas@users.noreply.github.com>
2020-11-09 15:28:19 -0800

    minor changes

--- convert_dir.py
+++ convert_dir.py
@@ -2,14 +2,8 @@ from audioconvert import *
 from sys import argv
 
 
-if len(argv) > 1:
-    dir = argv[1]
-else:
-    dir = input('What folder do you want to convert?\n')
-    auto_folder = input('What is the path of "Automatically Add to Music"?\n')
-
-auto_folder = '/Volumes/nathanbackup/Library/Automatically Add to Music.localized'
-
+dir = '/path/to/unconverted'
+auto_folder = '/path/to/auto'
 cues = get_cues(dir)
 split_cues(cues)
 convert_all_alac(dir)

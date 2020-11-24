109f028a1391c7ab18abbffe5f7f4708578349f8
pynathanthomas <74019033+pynathanthomas@users.noreply.github.com>
2020-11-24 00:43:17 -0800

    misc

--- __main__.py
+++ __main__.py
@@ -1,14 +1,17 @@
 from audioconvert import *
+import argparse
 
 dir = '/Volumes/nathanbackup/Downloads'
 auto_folder = '/Volumes/nathanbackup/Library/Automatically Add to Music.localized'
-music_dir = '/Volumes/nathanbackup/Music'
 
-cues = get_cues(dir)
-split_cues(cues)
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--split-cues', help='split large audio files based on cue', action='store_true')
+args = parser.parse_args()
+
+if args.split_cues:
+    cues = get_cues(dir)
+    split_cues(cues)
+
 convert_all_alac(dir)
 move_to_auto(dir, auto_folder)
-
-for d in listdir(dir):
-    system(f'mv "{dir}/{d}" "{music_dir}/{d}"')
 

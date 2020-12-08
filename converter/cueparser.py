b7fc62cf354f2b128f70cb5692389f72ca5004c1
pynathanthomas <74019033+pynathanthomas@users.noreply.github.com>
2020-12-07 16:46:06 -0800

    added cue class, WIP

--- converter/cueparser.py
+++ converter/cueparser.py
@@ -22,48 +22,100 @@ from pathlib import Path
 class Cue(object):
     def __init__(self, path):
         self.path = path
-        self.info = []
+        self._info = {
+            'album': None,
+            'albumartist': None,
+            'files': [],
+        }
         parent_dir = '/'.join(self.path.split('/')[:-1])
-        comments = []
-        album = ''
         lines = open(self.path).readlines()
 
         for l in lines:
             if l.startswith('REM'):
-            comments.append(l)
-            continue
-        elif l.startswith('PERFORMER'):
-            artist = _get_in_quotes(l)
-            continue
-        elif l.startswith('TITLE') and album == '':
-            album = _get_in_quotes(l)
-            continue
+                m = re.match(r'REM (\w+) "?([\w\d-]+)"?', l).groups()
+                self._info[m[0].lower()] = m[1]
+                continue
+            elif l.startswith('PERFORMER'):
+                self._info['albumartist'] = get_in_quotes(l)
+                continue
+            elif l.startswith('TITLE') and self._info['album'] is None:
+                self._info['album'] = get_in_quotes(l)
+                continue
 
-        elif l.startswith('FILE'):
-            info.append({
-                'artist': artist,
-                'album': album,
-                'filepath': parent_dir + '/' + _get_in_quotes(l),
-                'tracklist': [],
-                'timestamps': []
-            })
-            curr_file = len(files) - 1
+            elif l.startswith('FILE'):
+                self._info['files'].append({
+                    'artist': self._info['albumartist'],
+                    'album': self._info['album'],
+                    'filepath': parent_dir + '/' + get_in_quotes(l),
+                    'tracklist': [],
+                    'timestamps': []
+                })
+                curr_file = len(self._info['files']) - 1
 
-        # parse lines that are indented
-        l = l.strip()
-        if l.startswith('TITLE'):
-            files[curr_file]['tracklist'].append(_get_in_quotes(l))
-        elif l.startswith('INDEX'):
-            stamp = findall('\d\d:\d\d:\d\d', l)[0]
-            files[curr_file]['timestamps'].append(format_time(stamp))
+            # parse lines that are indented
+            l = l.strip()
+            if l.startswith('TITLE'):
+                self._info['files'][curr_file]['tracklist'].append(get_in_quotes(l))
+            elif l.startswith('INDEX'):
+                stamp = re.findall('\d\d:\d\d:\d\d', l)[0]
+                self._info['files'][curr_file]['timestamps'].append(format_time(stamp))
+            else:
+                continue
+
+    # input: str timestamp in format hh:mm:ss
+    # output: int timestamp in seconds
+    def format_time(stamp):
+        sl = stamp.split(':')
+        mins = int(sl[0][0]) * 10 + int(sl[0][1])
+        sec = int(sl[1][0]) * 10 + int(sl[1][1])
+        ms = int(sl[2][0]) * 10 + int(sl[2][1])
+        time = 60*mins + sec + ms / 100
+        return time
+
+    def format_query(self, q):
+        return ''.join(re.findall('\w', q)).lower()
+
+    def get(self, key):
+        if key == 'tracklist':
+            tracklist = []
+            stamps = []
+            for file in self._info['files']:
+                tracklist.extend(file['tracklist'])
+                stamps.extend(file['timestamps'])
+            return list(zip(tracklist, stamps))
+        elif key in ['artist', 'albumartist']:
+            return self._info['albumartist']
+        elif key in ['path', 'filepath', 'filepaths']:
+            return [file['filepath'] for file in self._info['files']]
         else:
-            continue
+            return self._info[key]
+
+    # TODO: add set function
+    # TODO: add remove function
 
 
 
-    def _get_in_quotes(s):
+
+
+    def get_in_quotes(s):
         match = re.findall('"([^"]+)"', s)[0]
         return match
+
+    def __getitem__(self, key):
+        return self.get(self.format_query(key))
+
+    def __setitem__(self, key, val):
+        self.set(self.format_query(key), val)
+
+    def __str__(self):
+        tracklist = []
+        for file in self._info['files']:
+            tracklist.extend(file['tracklist'])
+        tracks = '\n'.join(tracklist)
+        s = f'Album: {self.get("album")}\nArtist: {self.get("artist")}\n\nTracks:\n{tracks}'
+        return s
+
+
 
 
 
@@ -106,7 +158,7 @@ def parse_cue(cue_path):
             files[curr_file]['tracklist'].append(get_in_quotes(l))
         elif 'INDEX' in l:
             # finds timestamp
-            stamp = findall('\d\d:\d\d:\d\d', l)[0]
+            stamp = re.findall('\d\d:\d\d:\d\d', l)[0]
             files[curr_file]['timestamps'].append(format_time(stamp))
         else:
             continue
@@ -120,7 +172,7 @@ def parse_cue(cue_path):
 # input str: str to search
 # output str: str in quotes
 def get_in_quotes(s):
-    match = findall('"([^"]+)"', s)[0]
+    match = re.findall('"([^"]+)"', s)[0]
     return match
 
 # input: str timestamp in format hh:mm:ss
@@ -194,5 +246,4 @@ def split_file(in_file, out_file, start, end):
 
     command = f'ffmpeg -i "{in_file}" -map_metadata -1 -ss {start} {length_str} -vn -acodec alac -map 0:0 -y "{out_file}"'
     system(command)
-
 
